22700981ac04c9135d55b7002c394e636715c4b6
fc8880583 <fc8880583@gmail.com>
2018-05-02 01:02:27 -0500

    database

--- App1/App1/database.cs
+++ App1/App1/database.cs
@@ -7,6 +7,43 @@ namespace App1
 {
     class database
     {
+        public database()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "arezzo.database.windows.net";
+                builder.UserID = "qpan0831";
+                builder.Password = "Pan890831";
+                builder.InitialCatalog = "ArezzoData";
 
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT * FROM FoodAndWine");
+
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1} {2} {3}", reader.GetValue(0).ToString(), reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), reader.GetValue(3).ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
     }
 }
